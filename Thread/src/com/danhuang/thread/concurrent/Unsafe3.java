1c2fa9a238a33563f3e97a7fee1c7c639c31e08c
GodWithegg <675175647@qq.com>
2019-04-01 00:22:30 +0800

    线程不安全

--- Thread/src/com/danhuang/thread/concurrent/Unsafe3.java
+++ Thread/src/com/danhuang/thread/concurrent/Unsafe3.java
@@ -44,6 +44,14 @@ class Drawing extends Thread {
 
 	@Override
 	public void run() {
+		if (account.money - drawingMoney < 0) {
+			return;
+		}
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		account.money -= drawingMoney;
 		packetTotal += drawingMoney;
 		System.out.println(this.getName() + "-->ÕË»§Óà¶îÎª" + account.money);
