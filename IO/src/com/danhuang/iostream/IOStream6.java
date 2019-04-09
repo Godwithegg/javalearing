959d3e3afa7c72ceee3fdb8d0c992e23ad644857
GodWithegg <675175647@qq.com>
2019-04-10 00:10:09 +0800

    认识io流

--- IO/src/com/danhuang/iostream/IOStream6.java
+++ IO/src/com/danhuang/iostream/IOStream6.java
@@ -4,42 +4,36 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.Reader;
 import java.io.Writer;
 
 /**
- * 文件字符输出流 操作的步骤 1、创建源 2、选择流 3、操作 4、释放资源
+ * 文件字符输入流 操作的步骤 1、创建源 2、选择流 3、操作 4、释放资源
  * 
  * @author danhuang
  *
  */
-public class IOStream7 {
+public class IOStream6 {
 	public static void main(String[] args) {
 		// 1.创建源
 		File src = new File("label2.txt");
 		// 2.选择流
-		Writer writer = null;
+		Reader reader = null;
 		try {
-			writer = new FileWriter(src);
+			reader = new FileReader(src);
 			// 3.操作（读取）
-			String msg = "IO is so easy.";
-			
-			//写法一
-//			char[] datas = msg.toCharArray();
-//			writer.write(datas, 0, datas.length);
-//			writer.flush();
-
-			//写法二
-//			writer.write(msg);
-//			writer.flush();
-			
-			//写法三
-			writer.append(msg).append("I am handsome");
-			writer.flush();
-			
+			char[] flush = new char[1024];// 缓冲容器
+			int len = -1;// 接收长度
+			while ((len = reader.read(flush)) != -1) {
+				// 字符串组-->字符串
+				String str = new String(flush, 0, len);
+				System.out.println(str);
+			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -47,8 +41,8 @@ public class IOStream7 {
 		} finally {
 			// 4.释放资源
 			try {
-				if (writer != null) {
-					writer.close();
+				if (reader != null) {
+					reader.close();
 				}
 			} catch (IOException e) {
 				e.printStackTrace();
